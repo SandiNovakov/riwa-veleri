2607095352b3ff67dd6eac735707b783bfef44a7
Sandi Novakov <novakov.sandi@gmail.com>
2025-11-06 14:18:47 +0100

    ne znam

--- quasar-project/src/pages/TestPage.vue
+++ quasar-project/src/pages/TestPage.vue
@@ -1,12 +1,58 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Test Page</h1>
+  <q-page padding>
+    <q-card class="my-card bg-secondary text-white">
+      <q-card-section>
+        <div v-text="naslov"></div>
+        <div v-text="autor"></div>
+      </q-card-section>
 
-    <div>{{poruka}}</div>
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
 
+      <q-separator dark />
+
+      <q-card-actions>
+        <q-btn flat @click="openMsg()">Open</q-btn>
+        <q-btn flat @click="deleteMsg()">Delete</q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <div>
+      <q-input v-model="text" label="text"></q-input>
+      <q-input v-model="autor" label="autor"></q-input>
+      <q-input v-model="naslov" label="naslov"></q-input>
+
+      <q-btn flat @click="save()">save</q-btn>
+    </div>
   </q-page>
+ 
 </template>
 
 <script setup>
-const poruka = "Test page"
+import { ref } from 'vue';
+
+let naslov = ref('');
+let autor = ref('');
+let text  = ref('');
+
+var var_open = ref(4);
+var var_delete = ref(3);
+
+function save() {
+  console.log("naslov: "+naslov.value)
+  console.log("autor: "+autor.value)
+  console.log("text: "+text.value)
+}
+
+function openMsg() {
+  var_open.value = Math.max(var_open.value - 1, 0);
+
+  console.log("open: "+var_open.value);
+
+}
+function deleteMsg() {
+  var_delete.value = Math.max(var_delete.value - 1, 0);
+  console.log("delete: "+var_delete.value);
+}
 </script>
